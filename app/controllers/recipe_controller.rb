82bce0efd3a16622cf70ff4bdf7740668f2424d2
BiancaMe <bianquis0410@gmail.com>
2024-01-31 18:38:40 -0300

    Add fuctions to display the public list of recipes

---
+++ app/controllers/recipe_controller.rb
@@ -0,0 +1,13 @@
+class RecipeController < ApplicationController
+    def public_index
+        @recipes = Recipe.includes(:recipe_foods, recipe_foods: :food).where(public: true)
+    end
+
+    def destroy 
+        @recipe = Recipe.find(params[:id])
+        authorize! :destroy, @recipe
+        @recipe.destroy
+    end
+
+end
+
