dcc3ea203589fafa28138261fff9d04cf16e2162
BiancaMe <bianquis0410@gmail.com>
2024-01-31 18:39:49 -0300

    Add method to calculate the total price of a public recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,14 @@ class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User'
 
   has_many :recipe_foods, foreign_key: :recipe_id
+
+  
+  def total_price
+    total = 0
+    recipe_foods.each do |r_food|
+    total = total + r_food.quantity*r_food.food.price
+    end
+    total
+  end
+
 end
