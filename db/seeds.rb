aa8e356a4916532c0b454ddfd0281332adf8123c
BiancaMe <bianquis0410@gmail.com>
2024-02-02 14:38:55 -0300

    Update to work on Shopping list

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,21 +13,22 @@ second_user = User.create(name: 'Lilly')
 tomato = Food.create(name: 'Tomato',measurement_unit: 'g' , price: 2)
 potato = Food.create(name: 'Potato',measurement_unit: 'g' , price: 2)
 apple = Food.create(name: 'Apple',measurement_unit: 'g' , price: 3)
+milk = Food.create(name: 'Milk',measurement_unit: 'g' , price: 1)
 
 
 
 pure = Recipe.create(name: 'pure',preparation_time: 0.5,cooking_time: 20, description: 'Pure de papa', public: true,user: first_user)
 soap = Recipe.create(name: 'soap',preparation_time: 0.5,cooking_time: 30, description: 'Sopa de tomate', public: true,user: second_user)
 
-rf2 = RecipeFood.create(quantity:200,recipe_id: soap.id,food_id:tomato.id)
+
+rf3 = RecipeFood.create(quantity:200,recipe_id: soap.id,food_id:tomato.id)
+rf2 = RecipeFood.create(quantity:50,recipe_id: pure.id,food_id: milk.id)
 rf1 = RecipeFood.create(quantity:400,recipe_id: pure.id,food_id:potato.id)
 soap2 = Recipe.create(name: 'soap potato',preparation_time: 0.5,cooking_time: 30, description: 'Sopa de papa', public: false,user: second_user)
 
 
-inv1 = Inventory.create(name: 'Inventory 1', user: first_user)
-inv2 = Inventory.create(name: 'Inventory 2', user: first_user)
+inv1 = Inventory.create(name: 'Inventory 1',description:'First inventory', user_id: first_user.id)
+inv2 = Inventory.create(name: 'Inventory 2',description:'Second inventory', user: first_user)
 
-invf = InventoryFood.create(quantity: 100,inventory_id: inv1 , food: tomato)
-invf1 = InventoryFood.create(quantity: 100, inventory: in1, food: tomato)
+invf1 = InventoryFood.create(quantity: 100,inventory: inv1 , food: potato)
 
-inventory_food = InventoryFood.create(quantity: 100, inventory: inventory.id, food: food.id)
