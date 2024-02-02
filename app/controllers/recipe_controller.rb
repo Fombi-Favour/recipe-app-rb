f892749fe073447b24b73aced984a7a91df3feb2
BiancaMe <bianquis0410@gmail.com>
2024-02-02 04:56:07 -0300

    Add method shopping list

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -7,16 +7,42 @@ class RecipeController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
-  def modal 
+  def modal
     @recipe = Recipe.find(params[:id])
     @inventories = Inventory.all
   end
 
-  def shopping_list 
-    @recipe = Recipe.find(params[:id])
+  def shopping_list
+    @recipe = Recipe.includes(recipe_foods: :food).find(params[:id])
     @inventory = Inventory.find(params[:inventory_id])
+
+    @shop_list = []
+    @recipe.recipe_foods.each do |rf| 
+      i_food =  @inventory.inventory_foods.find_by(food: rf.food.id)
+
+      if i_food.nil?
+        quantity = rf.quantity
+        shop_price = quantity *  rf.food.price
+      else
+        if i_food.quantity < rf.quantity
+          quantity = rf.quantity - i_food.quantity
+          shop_price = quantity *  rf.food.price
+        else
+          quantity = 0
+        end
+      end
+
+      if quantity > 0 
+        @shop_list << {
+          name: rf.food.name,
+          quantity:,
+          measurement_unit: rf.food.measurement_unit,
+          shop_price:
+        }
+      end
+    end
   end
-  
+
   def destroy
     @recipe = Recipe.find(params[:id])
     authorize! :destroy, @recipe
