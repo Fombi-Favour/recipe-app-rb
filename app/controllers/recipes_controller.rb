ed8531b466567ae04160a98163beddf2762c26fd
Fombi Favour <fombifavour@gmail.com>
2024-02-02 09:54:44 +0100

    Update recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,23 @@ class RecipesController < ApplicationController
   def show
     @recipes = Recipe.find(params[:id])
   end
+
+  def new
+    @recipes = Recipe.new
+  end
+
+  def create
+    @recipes = Recipe.new(recipe_params)
+    if @recipes.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created'
+    else
+      render :new, notice: 'Recipe was not created'
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipes).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
