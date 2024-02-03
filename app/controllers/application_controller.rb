c7913701945ba1da50ded233b974f7d4458dfb54
BiancaMe <bianquis0410@gmail.com>
2024-02-03 10:11:34 -0300

    Set up and views for devise completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+    before_action :authenticate_user!
 
-    protected 
+    protected
 
     def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :bio, :email, :password, :current_password)
+      end
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,17 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => '[redacted]',
+    :user_name            => '[redacted]',
+    :password             => '[redacted]',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 
   # Do not eager load code on boot.
   config.eager_load = false
