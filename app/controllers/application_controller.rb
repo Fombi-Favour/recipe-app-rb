f9323692b4abad57921f320feedd3e5c0fc71a0e
BiancaMe <bianquis0410@gmail.com>
2024-02-03 10:40:20 -0300

    Add style and fix linters errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    protected
+  protected
 
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :bio, :email, :password, :current_password)
-      end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password)
     end
+  end
 end
