6e05e4ed688a90b94df5aa6fd0f6e3e1348b9653
BiancaMe <bianquis0410@gmail.com>
2024-02-03 17:35:34 -0300

    Add test for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is valid with valid attributes' do
+    food = Food.new(name: 'Apple',measurement_unit: 'g', price: 2)
+    expect(food).to be_valid
+  end
+
+  it 'validate that has many recipe foods' do
+    res = described_class.reflect_on_association(:recipe_foods)
+    expect(res.macro).to eq :has_many
+  end
+
+  it ' validate that has many inventory foods' do
+    res = described_class.reflect_on_association(:inventory_foods)
+    expect(res.macro).to eq :has_many
+  end
 end
